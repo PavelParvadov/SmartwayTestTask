7e000bb452d3d78d8923ace65882542b9ed79dbc
PavelParvadov <parvadov.pavel@gmail.com>
2025-10-08 20:54:15 +0300

    add: индексы и данные добавлены

---
+++ migrations/002_add_data.up.sql
@@ -0,0 +1,25 @@
+CREATE INDEX ON employees(company_id);
+CREATE INDEX ON employees(department_id);
+CREATE INDEX ON passports(number,  type);
+CREATE INDEX ON employees(passport_id);
+
+INSERT INTO companies (name) VALUES
+     ('Smartway'),
+     ('T-bank');
+
+INSERT INTO passports (type, number) VALUES
+    ('5621', '323232'),
+    ('5622', '611111'),
+    ('5623', '777777'),
+    ('5624', '332222');
+
+INSERT INTO departments (name, phone, company_id) VALUES
+     ('IT', '+7 953 022 15 68', 1),
+     ('HR', '8-800-555-35-35', 2),
+     ('Support', '89005553535', 1);
+
+INSERT INTO employees (name, surname, phone, company_id, passport_id, department_id) VALUES
+      ('Павел',     'Парвадов', '+7-202-555-0140', 1, 1, 1),
+      ('Ксения',   'Доронина', '+7-202-555-0172', 2, 2, 2),
+      ('Дональд',     'Трамп',    '+7-202-555-0114', 1, 3, 3),
+      ('Игорь', 'Стрелков',    '+7-202-555-0183', 2, 4, 2);
---
+++ migrations/002_delete.down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS employees_company_id_idx;
+DROP INDEX IF EXISTS employees_department_id_idx;
+DROP INDEX IF EXISTS passports_number_type_idx;
+DROP INDEX IF EXISTS employees_passport_id_idx;
