eb187e736604bfccf231ae0e7851c89d1e07f3f3
PavelParvadov <parvadov.pavel@gmail.com>
2025-10-09 19:52:22 +0300

    fix: small fix

--- internal/domain/models/employee.go
+++ internal/domain/models/employee.go
@@ -1,11 +1,12 @@
 package models
 
 type Employee struct {
-	ID         int        `json:"id"`
-	Name       string     `json:"name"`
-	Surname    string     `json:"surname"`
-	Phone      string     `json:"phone"`
-	CompanyId  int        `json:"company_id"`
-	Passport   Passport   `json:"passport"`
-	Department Department `json:"department"`
+	ID           int        `json:"id"`
+	Name         string     `json:"name"`
+	Surname      string     `json:"surname"`
+	Phone        string     `json:"phone"`
+	CompanyId    int        `json:"company_id"`
+	DepartmentId int        `json:"department_id"`
+	Passport     Passport   `json:"passport"`
+	Department   Department `json:"department"`
 }
